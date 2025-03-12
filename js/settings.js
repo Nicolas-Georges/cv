4a1c00602f4aaf7151b245d56558df9682750988
Nicolas-Georges Willems <113925172+Nicolas-Georges@users.noreply.github.com>
2025-03-12 08:51:29 +0100

    add css smartphone 1

---
+++ js/settings.js
@@ -0,0 +1,12 @@
+export const settings= {
+    classHidden:'hidden',
+    classActive:'active',
+    classSectionPrimaryMenu:'.section-primary-menu',
+    idNavReseaux:'nav-reseaux',
+    idBoutonReseaux:'button-reseaux',
+    idBurgerMenu:'burger-menu',
+    idPrimaryMenu:'primary-menu',
+    idNavBurgerMenu:'nav-burger-menu',
+    idCloseBurgerMenu:'close-burger-menu',
+    idCloseReseauxMenu:'close-reseaux-menu',
+}
